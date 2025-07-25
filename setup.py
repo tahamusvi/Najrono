88e70540638bbe40a9daa5bef301f5b011b8d2e5
Taha <taham8000@gmail.com>
2025-07-25 22:43:27 +0330

    0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='najrono',
-    version='0.0.71',
+    version='0.0.8',
     packages=find_packages(),
     install_requires=[
         'jalali_core==1.0.0',
